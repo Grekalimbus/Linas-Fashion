13680d14539ae6f80fd718bd815dbc35df005bfd
Danil <grechkindanil322@gmail.com>
2023-12-31 00:47:27 +0100

    Fixed responsobility in FirstDisplay page. Gonna complete it and implemented display depend of screen

--- app/components/FirstDisplay.jsx
+++ app/components/FirstDisplay.jsx
@@ -5,8 +5,8 @@ const FirstDisplay = () => {
     <div className="w-full mx-auto h-svh bg-[#04111b] pt-10">
       <div className="max-w-7xl mx-auto">
         <div className="h-full flex justify-between">
-          <div className="flex flex-col">
-            <div className="flex space-x-3">
+          <div className="flex flex-col justify-between">
+            <div className="flex space-x-3 md:w-6/12">
               <div>
                 <img
                   src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=297,h=201,fit=crop/linasyfhpf/Home_1-mjEDk3krBEINqzOl.png"
@@ -20,13 +20,20 @@ const FirstDisplay = () => {
                 />
               </div>
             </div>
-            <span style={{ "font-size": "250px" }}>Linas</span>
-            <div className="flex">
-              <div>FASHION PHOTOGRAPHY | AMSTERDAM</div>
-              <div>THOEMKE@LINAS.COM</div>
+            <div>
+              <span
+                className="font-bold word-wrap block"
+                style={{ "font-size": "220px" }}
+              >
+                Linas
+              </span>
+              <div className="flex justify-between">
+                <div>FASHION PHOTOGRAPHY | AMSTERDAM</div>
+                <div>THOEMKE@LINAS.COM</div>
+              </div>
             </div>
           </div>
-          <div>
+          <div className="w-6/12">
             <img
               src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=503,h=673,fit=crop/linasyfhpf/Home_3-YBgxMPMkXLHyZX5g.png"
               alt="image3"
