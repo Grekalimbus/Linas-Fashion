25fd3ac2c60877162a42dbe85e40cee5408c0b05
Danil <grechkindanil322@gmail.com>
2023-12-31 21:02:09 +0100

    fix display for header, Nav and component FirstDisplay. Also a little fixed SecondSidplay component. Need clear code and implement follow features

--- app/components/FirstDisplay.jsx
+++ app/components/FirstDisplay.jsx
@@ -1,7 +1,8 @@
-const FirstDisplay = ({ valueDisplay }) => {
-  // create funck to generate number value. depend on that mappin display
-  return valueDisplay <= 950 ? (
-    <div className="w-full flex justify-center items-center mx-auto h-svh bg-[#04111b] pt-10">
+const FirstDisplay = ({ screen, screenWidth }) => {
+  return screenWidth <= 950 || screen <= 950 ? (
+    <div
+      className={`w-full flex justify-center items-center mx-auto h-svh bg-[#04111b] pt-10`}
+    >
       <div className="flex flex-col max-w-[360px] justify-center items-center">
         <h3 className="font-bold word-wrap block" style={{ fontSize: "80px" }}>
           Linas
@@ -9,20 +10,24 @@ const FirstDisplay = ({ valueDisplay }) => {
         <p>FASHION PHOTOGRAPHY | AMSTERDAM</p>
         <p>THOEMKE@LINAS.COM</p>
         <img
+          className="my-4"
           src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=503,h=673,fit=crop/linasyfhpf/Home_3-YBgxMPMkXLHyZX5g.png"
           alt="image3"
         />
-        <div className="w-full flex justify-between">
-          <div>
+        <div className="w-full flex justify-between ">
+          <div className="w-[48%]">
             <img
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=297,h=201,fit=crop/linasyfhpf/Home_1-mjEDk3krBEINqzOl.png"
-              alt="image1"
-            />
-          </div>
-          <div>
-            <img
+              className="w-full h-auto"
               src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=194,h=201,fit=crop/linasyfhpf/Home_2-m6L1x6xeZetk65zO.png"
               alt="image2"
+            />
+          </div>
+
+          <div className="w-[48%]">
+            <img
+              className="w-full h-full"
+              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=297,h=201,fit=crop/linasyfhpf/Home_1-mjEDk3krBEINqzOl.png"
+              alt="image1"
             />
           </div>
         </div>
