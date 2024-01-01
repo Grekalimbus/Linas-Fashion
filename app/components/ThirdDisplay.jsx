a4e181fe62f1ea45944f79bb42b4b14384dee9f6
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2024-01-01 03:25:27 +0100

    ready third-display to all devises

--- app/components/ThirdDisplay.jsx
+++ app/components/ThirdDisplay.jsx
@@ -1,5 +1,36 @@
-const ThirdDisplay = () => {
-  return (
+const ThirdDisplay = ({screenWidth, styleScreen}) => {
+  return screenWidth <= 950 || styleScreen === "w-[360px]" ? (
+    <div className="flex justify-center items-center w-full bg-[#04111b]">
+      <div className="mt-20">
+        <h1 className="text-white font-bold text-6xl ">02</h1>
+        <h4 className="text-white font-medium pt-5 ">FLOWER GARDEN</h4>
+        <a className="text-white font-medium underline cursor-pointer pt-3">
+          Learn more about the project →
+        </a>
+        <img
+          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1006,h=1582,fit=crop/linasyfhpf/Home_8-m5KgxNx9kwhwwqNW.png"
+          alt="ManModel2"
+          className="w-[327px] h-[625px] pt-10"
+        />
+        <img
+          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=984,h=1034,fit=crop/linasyfhpf/Home_6-YyvLQNQb5bHZrQ2V.png"
+          alt="flower"
+          className="w-[235px] h-[245px] ml-[92px] pt-5"
+        />
+        <img
+          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=984,h=1410,fit=crop/linasyfhpf/Home_7-AE0xXDXkxGuLqpgk.png"
+          alt="ModelMan"
+          className="w-[263px] h-[371px] pt-5"
+        />
+        <div className="flex p-5">
+          <p className="text-white text-sm ml-10">
+            MUA: Jessie <br /> Gein
+          </p>
+          <p className="text-white text-sm ml-[92px] ">Model: Jake James</p>
+        </div>
+      </div>
+    </div>
+  ) : (
     <div className="mx-auto justify-center bg-[#04111b] pt-5 relative flex">
       <div className="w-full container static mt-10 flex justify-center">
         <div>
