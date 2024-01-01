5821c7e5504cfd1ab32bb00b3a84a88335c25a4a
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2024-01-01 02:45:10 +0100

    ready responsive design to sm devises

--- app/components/ThirdDisplay.jsx
+++ app/components/ThirdDisplay.jsx
@@ -1,36 +1,36 @@
 const ThirdDisplay = () => {
   return (
-    <div className="container-2xl mx-auto bg-black xl:flex xl:justify-center ">
-      <div className="mt-20  relative">
-        <div className="xl:flex mt-10 ml-[110px] ">
-          <h1 className="color-white text-bold text-9xl w-[20px]">02</h1>
-          <img
-            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=804,h=1152,fit=crop/linasyfhpf/Home_7-AE0xXDXkxGuLqpgk.png"
-            alt="ModelMan"
-            className=" w-[402px] h-[576px] z-[0] static ml-[140px] mt-20"
-          />
-          <img
-            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=594,h=624,fit=crop/linasyfhpf/Home_6-YyvLQNQb5bHZrQ2V.png"
-            alt="flower"
-            className="w-[297px] h-[312px] absolute z-20 mt-[130px] mr-[90px] "
-          />
+    <div className="mx-auto justify-center bg-[#04111b] pt-5 relative flex">
+      <div className="w-full container static mt-10 flex justify-center">
+        <div>
+          <h1 className="color-white font-bold text-8xl w-[10%] ml-5">02</h1>
+          <div className="flex relative">
+            <img
+              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=984,h=1410,fit=crop/linasyfhpf/Home_7-AE0xXDXkxGuLqpgk.png"
+              alt="ManModel"
+              className="xl:w-[402px] xl:h-[576px] mt-20 ml-[190px] z-[0] static lg:w-[361px] lg:h-[517px] md:w-[307px] md:h-[440px] md:mt-5"
+            />
+            <img
+              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=984,h=1034,fit=crop/linasyfhpf/Home_6-YyvLQNQb5bHZrQ2V.png"
+              alt="Flower"
+              className="absolute  w-[297px] h-[312px] z-10 flex  mt-[265px] lg:w-[266px] lg:h-[280px] md:w-[227px] md:h-[238px] md:mt-[180px] md:mr-20 "
+            />
+            <div className="z-10 absolute bottom-0">
+              <h4 className="color-white text-medium">FLOWER GARDEN</h4>
+              <a className="underline color-white font-medium">
+                Learn more about the project →
+              </a>
+            </div>
+            <img
+              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1006,h=1582,fit=crop/linasyfhpf/Home_8-m5KgxNx9kwhwwqNW.png"
+              alt="ModelMan2"
+              className="xl:w-[503px] xl:h-[791px] mt-5 lg:w-[452px] lg:h-[710px] ml-[100px] md:w-[385px] md:h-[605px]static z-[0] "
+            />
+            <p className="font-sans color-white mr-5  mb-5 flex absolute right-0 bottom-0 shadow-sm">
+              MUA: Jessie Gein
+            </p>
+          </div>
         </div>
-        <div className="justify-start mt-10 ml-[110px]">
-          <h2 className="color-white font-medium">FLOWER GARDEN</h2>
-          <a className="underline color-white font-medium tracking-wide cursor-pointer">
-            Learn more about the project →
-          </a>
-        </div>
-      </div>
-      <div className="mt-20 ml-[140px] relative ">
-        <img
-          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1006,h=1582,fit=crop/linasyfhpf/Home_8-m5KgxNx9kwhwwqNW.png"
-          alt="ManModel2"
-          className="static w-[503px] h-[791px] z-0"
-        />
-        <p className="color-white absolute font-medium z-10 bottom-10 right-10 ">
-          MUA: Jessie Gein
-        </p>
       </div>
     </div>
   );
