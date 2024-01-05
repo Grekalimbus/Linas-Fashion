126938b9c73767db02967dc9da8c2ed9925d9041
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2024-01-05 23:55:24 +0100

    fix fourth display component in components , bug with heigh page

--- app/components/FourthDisplay.jsx
+++ app/components/FourthDisplay.jsx
@@ -22,7 +22,7 @@ const FourthDisplay = ({screen, screenWidth}) => {
           <img
             src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1080,h=1445,fit=crop/linasyfhpf/Home_9-Y4Lg6q6QPvt7bRnp.png"
             alt="Model1"
-            className="w-[328px] h-[439px] mt-20"
+            className="w-[328px] h-[439px] mt-20 object-contain"
           />
           <p className="font-sansi text-black flex justify-end mt-10">
             Model: Yoko Yamada
@@ -33,52 +33,47 @@ const FourthDisplay = ({screen, screenWidth}) => {
           <img
             src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1080,h=915,fit=crop/linasyfhpf/project3_2-YZ9p0JjL47HE1gJj.png"
             alt="Model2"
-            className="w-[328px] h-[278px] pb-5"
+            className="w-[328px] h-[278px] pb-5 object-contai"
           />
           <img
             src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1080,h=915,fit=crop/linasyfhpf/project3_3-m2Wk5vEOzjUO3MX5.png"
             alt="Model3"
-            className="w-[328px] h-[278px]"
+            className="w-[100%] h-[278px] object-contain"
           />
         </div>
       </div>
     </div>
   ) : (
-    <div className="mx-auto flex justify-center items-start ">
-      <div
-        ref={targetRef}
-        className="w-full container flex justify-center static   mt-10"
-      >
-        <div className="relative flex mt-20 max-w-6xl">
-          <h1 className="text-black text-8xl font-sans font-medium z-[1] absolute pt-10 pl-10">
+    <div className="mx-auto w-full bg-white flex justify-center">
+      <div className="container w-full max-w-6xl flex justify-center">
+        <div className="mt-20 mb-20 relative flex">
+          <h1 className="text-black text-8xl font-semibold absolute z-[1] ml-10 mt-20">
             03
           </h1>
           <img
             src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1418,h=1740,fit=crop/linasyfhpf/Home_9-Y4Lg6q6QPvt7bRnp.png"
-            alt="Model1"
-            className="w-[100%] h-[65%] z-0 static"
+            alt="pic1"
+            className="w-[710px] h-[870px] static object-contain"
           />
-          <div className="ml-20 ">
-            <p className="text-black font-sans font-medium flex justify-end">
+          <div className="ml-[120px] mt-10">
+            <h2 className="text-black font-light">
               Model: Yoko Yamada
-            </p>
-            <p className="text-black font-sans font-medium flex justify-end">
-              MUA: Jessie Gein
-            </p>
+              <br /> MUA: Jessie Gein
+            </h2>
             <img
               src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=800,h=548,fit=crop/linasyfhpf/project3_2-YZ9p0JjL47HE1gJj.png"
-              alt="Model2"
-              className="w-[98%] mb-10 pt-10 object-contatin"
+              alt="img1"
+              className="w-[400px] h-[234px] mt-3"
             />
             <img
               src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=800,h=744,fit=crop/linasyfhpf/project3_3-m2Wk5vEOzjUO3MX5.png"
-              alt="Model3"
-              className="w-[98%] h-[38%] object-contatin"
+              alt="img2"
+              className="w-[400px] h-[410px] object-contain mt-2"
             />
-            <p className="font-semibold text-black font-sans mt-[65px]">
+            <h2 className="font-semibold text-black text-[17px] mt-2">
               SOMETHING IN THE AIR
-            </p>
-            <p className="font-medium text-black font-sans underline">
+            </h2>
+            <p className="text-black font-light text-[17px] underline mt-5">
               Learn more about the project →
             </p>
           </div>
