6d4fde66a3c776a6d1d542c25ccbdebd377c1312
Danil <grechkindanil322@gmail.com>
2024-01-05 01:33:26 +0100

    finished link about. change global.css to added more kind of animations. hook uncompleted yet

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -12,7 +12,10 @@ const Footer = ({ screen, screenWidth }) => {
         className="flex flex-col max-w-[420px] justify-center items-center py-10 font-semibold"
       >
         <p>LINAS THOEMKE </p>
-        <p className="mt-4 border-b-2 border-white">THOEMKE@LINAS.COM</p>
+        <p className="relative mt-4">
+          THOEMKE@LINAS.COM
+          <span className="self-end w-[100%] absolute left-0 right-0 bottom-1 h-[1px] bg-white transform"></span>
+        </p>
         <p className="mt-2">+3145683245</p>
         <div className="w-[60%] flex justify-between mt-6">
           <FaTwitter className="w-[25px] h-[25px]"></FaTwitter>
@@ -23,11 +26,14 @@ const Footer = ({ screen, screenWidth }) => {
     </div>
   ) : (
     <div className="w-full mx-auto bg-[#04111b] p-16">
-      <div ref={targetRef} className="max-w-7xl mx-auto flex justify-between ">
-        <div className="flex justify-between w-[50%] font-semibold">
+      <div ref={targetRef} className="max-w-6xl mx-auto flex justify-between ">
+        <div className="flex justify-between w-[50%] font-semibold relative">
           <p>LINAS THOEMKE </p>
           <p>+3145683245</p>
-          <p className="border-b-2 border-white">THOEMKE@LINAS.COM</p>
+          <p className="relative">
+            THOEMKE@LINAS.COM
+            <span className="self-end w-[100%] absolute left-0 right-0 bottom-1 h-[2px] bg-white transform"></span>
+          </p>
         </div>
         <div className="flex justify-between w-[15%]">
           <FaTwitter className="w-[25px] h-[25px]"></FaTwitter>
