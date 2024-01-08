9136d19b6532133a1c4bea0b23431f30b22e2238
Данил <105842534+Grekalimbus@users.noreply.github.com>
2024-01-08 08:26:08 +0100

    fix

--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -5,12 +5,12 @@ import Footer from "../components/Footer";
 import useScreenHandling from "../hooks/useScreenHandling";
 
 const page = () => {
-  const { wrapperStyle, screen } = useScreenHandling();
+  const { screen } = useScreenHandling();
 
   return (
     <div className="flex overflow-hidden max-h-screen flex-col w-full items-center">
       <div
-        className={`${wrapperStyle} overflow-auto max-h-screen transition-all duration-500`}
+        className={`w-full overflow-auto max-h-screen transition-all duration-500`}
       >
         <Navbar screen={screen} />
         <ContextBlock screen={screen} />
