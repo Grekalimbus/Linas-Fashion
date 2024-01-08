26df4b1520f206478756b5d2f2a5fadd2d801a31
Данил <105842534+Grekalimbus@users.noreply.github.com>
2024-01-08 14:10:29 +0100

    refactor

--- app/about/FirstBlockContent.jsx
+++ app/about/FirstBlockContent.jsx
@@ -1,83 +1,55 @@
-import { useRef } from "react";
-import useIntersectionObserver from "../hooks/useIntersectionObserver";
-
-const FirstBlockContent = ({ screen }) => {
-  const targetLarg = useRef(null);
-  const targetMedium = useRef(null);
-
-  const animationsAndRefs = {
-    refsArray: [targetLarg, targetMedium],
-    animationNamesArray: ["animate-slideUp-large", "animate-slideUp-medium"],
-  };
-
-  useIntersectionObserver(animationsAndRefs);
-
-  return screen <= 1152 ? (
-    <div className="w-full flex justify-center mx-auto min-h-min bg-[#04111b] text-white">
-      <div
-        ref={targetLarg}
-        className="container flex flex-col max-w-[420px] justify-center p-8"
-      >
-        <h3 className="font-bold word-wrap block text-5xl">Capturing magic</h3>
-        <p>LINAS THOEMKE | </p>
-        <p>FASHION </p>
-        <p>PHOTOGRAPHY </p>
-        <img
-          className="mt-10"
-          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=984,h=1213,fit=crop/linasyfhpf/about_1-AoP1yOwKMzh26ZLV.png"
-          alt="image"
-        />
-        <div className="w-full flex justify-between mt-4">
-          <div className="w-[48%]">
-            <img
-              className="w-full h-auto"
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=945,h=963,fit=crop/linasyfhpf/about_2-YZ9p0JvgNqUp30OP.png"
-              alt="image2"
-            />
-          </div>
-
-          <div className="w-[48%]">
-            <img
-              className="w-full h-full"
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=945,h=963,fit=crop/linasyfhpf/about_3-m7VjgnGXpkhrMQN5.png"
-              alt="image1"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  ) : (
+import AnimateWrap from "../components/AnimateWrap";
+const FirstBlockContent = () => {
+  const images = [
+    "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=984,h=1213,fit=crop/linasyfhpf/about_1-AoP1yOwKMzh26ZLV.png",
+    "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=945,h=963,fit=crop/linasyfhpf/about_2-YZ9p0JvgNqUp30OP.png",
+    "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=945,h=963,fit=crop/linasyfhpf/about_3-m7VjgnGXpkhrMQN5.png",
+  ];
+  return (
     <div className="mx-auto bg-[#04111b] w-full flex justify-center items-center pb-16">
-      <div className="container max-w-6xl flex justify-around relative mt-12">
-        <img
-          ref={targetLarg}
-          className="w-[48%] h-[76vh] object-cover"
-          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=984,h=1213,fit=crop/linasyfhpf/about_1-AoP1yOwKMzh26ZLV.png"
+      <div className="container max-w-6xl flex justify-around mt-12 max-[1124px]:flex-col max-[1124px]:items-center">
+        <AnimateWrap
+          tag="img"
+          className="w-[48%] h-[76vh] object-cover max-[1124px]:hidden"
+          src={images[0]}
           alt="image1"
         />
 
-        <div ref={targetMedium} className="ml-20">
-          <div className="h-[28%] flex justify-center">
+        <div className="ml-20 max-[1124px]:ml-0 flex flex-col max-[1124px]:max-w-[340px] items-center">
+          <AnimateWrap
+            tag="img"
+            className="hidden h-[47vh] object-cover max-[1124px]:order-2 max-[1124px]:block my-8"
+            src={images[0]}
+            alt="image1"
+          />
+          <AnimateWrap
+            tag="div"
+            className="h-[28%] flex justify-center max-[1124px]:order-3"
+          >
             <img
               className="max-w-[42%] object-cover"
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=945,h=963,fit=crop/linasyfhpf/about_2-YZ9p0JvgNqUp30OP.png"
+              src={images[1]}
               alt="image2"
             />
 
             <img
               className="ml-4 max-w-[100%] min-w-[52%] bg-cover bg-right-bottom	object-cover"
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=945,h=963,fit=crop/linasyfhpf/about_3-m7VjgnGXpkhrMQN5.png"
+              src={images[2]}
               alt="image3"
             />
-          </div>
-          <div>
-            <h3 className="text-8xl font-semibold mt-20">Capturing </h3>
+          </AnimateWrap>
+          <AnimateWrap tag="div" className="max-[1124px]:order-1">
+            <h3 className="text-8xl font-semibold mt-20  max-[1124px]:text-[50px] max-[1124px]:mt-6 max-[1124px]:font-bold">
+              Capturing
+            </h3>
 
-            <h3 className="text-8xl font-semibold mt-2 ml-10">magic</h3>
-            <p className="text-1xl mt-44 ml-6">
+            <h3 className="text-8xl font-semibold mt-2 ml-10  max-[1124px]:text-[50px] max-[1124px]:ml-0 max-[1124px]:font-bold">
+              magic
+            </h3>
+            <p className="text-1xl mt-44 ml-6 max-[1124px]:mt-12">
               LINAS THOEMKE | FASHION PHOTOGRAPHY
             </p>
-          </div>
+          </AnimateWrap>
         </div>
       </div>
     </div>
--- app/about/SecondBlockContent.jsx
+++ app/about/SecondBlockContent.jsx
@@ -1,112 +1,95 @@
-import { useRef } from "react";
 import Link from "next/link";
-import useIntersectionObserver from "../hooks/useIntersectionObserver";
 
-const SecondBlockContent = ({ screen }) => {
-  const targetMedium = useRef(null);
-
-  const animationsAndRefs = {
-    refsArray: [targetMedium],
-    animationNamesArray: ["animate-slideUp-medium"],
-  };
-
-  useIntersectionObserver(animationsAndRefs);
-
-  return screen <= 1152 ? (
-    <div className="w-full flex justify-center mx-auto text-black min-h-min">
-      <div
-        ref={targetMedium}
-        className="flex flex-col items-start justify-start max-w-[420px] p-8 opacity-0"
-      >
-        <p className="block mt-4 text-2xl text-black">
-          Linas Thoemke is a fashion photographer based in Amsterdam. He started
-          working for small indie publications back in the mid-00s, and gathered
-          a following that slowly attracted the attention of bigger brands.
-        </p>
-        <br></br>
-        <p className="block mt-4 text-2xl">
-          Linas has worked on a wide array of different projects. Available to
-          work worldwide.
-        </p>
-        <Link
-          href="https://linasyfhpf.zyrosite.com/clients"
-          className="font-medium mt-4 text-2xl relative"
-        >
-          CLIENT LIST →
-          <span className="absolute left-0 right-0 bottom-1 h-[2px] bg-black transform"></span>
-        </Link>
-        <p className="font-medium text-2xl mt-12">EXHIBITIONS</p>
-        <div className="mt-10 ">
-          <p>THIS IS LINAS: ORIGIN STORY</p>
-          <p>2008, Amsterdam</p>
-        </div>
-
-        <div className="mt-10">
-          <p>TAKES ON COLOR AND SEA</p>
-          <p>2010, London and Amsterdam</p>
-        </div>
-
-        <div className="mt-10">
-          <p>EXPRESSIONS OF MORTALITY</p>
-          <p>2011, Amsterdam</p>
-        </div>
-
-        <div className="mt-10">
-          <p>THOEMKE & GEET: EXPLORING </p>
-          <p>2015-2016, Amsterdam and Paris</p>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className="mx-auto w-full flex justify-center items-center text-black pt-10 pb-32">
-      <div
-        ref={targetMedium}
-        className="container max-w-6xl flex justify-between  relative mt-12 opacity-0 "
-      >
-        <div className="w-[37%]">
-          <p className="block mt-4 text-2xl">
-            Linas Thoemke is a fashion photographer based in Amsterdam. He
-            started working for small indie publications back in the mid-00s,
-            and gathered a following that slowly attracted the attention of
-            bigger brands.
-          </p>
-          <br></br>
-          <p className="block mt-4 mb-4 text-2xl">
-            Linas has worked on a wide array of different projects. Available to
-            work worldwide.
-          </p>
-          <Link
-            href="https://linasyfhpf.zyrosite.com/clients"
-            className="font-medium border-b-2 border-black text-2xl "
-          >
-            CLIENT LIST →
-          </Link>
-        </div>
-        <div className="w-[30%] mr-36 mt-2">
-          <p className="font-medium text-2xl">EXHIBITIONS</p>
-          <div className="mt-10 ">
-            <p>THIS IS LINAS: ORIGIN STORY</p>
-            <p>2008, Amsterdam</p>
-          </div>
-
-          <div className="mt-10">
-            <p>TAKES ON COLOR AND SEA</p>
-            <p>2010, London and Amsterdam</p>
-          </div>
-
-          <div className="mt-10">
-            <p>EXPRESSIONS OF MORTALITY</p>
-            <p>2011, Amsterdam</p>
-          </div>
-
-          <div className="mt-10">
-            <p>THOEMKE & GEET: EXPLORING </p>
-            <p>2015-2016, Amsterdam and Paris</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+const SecondBlockContent = () => {
+  // return screen <= 1152 ? (
+  //   <div className="w-full flex justify-center mx-auto text-black min-h-min">
+  //     <div
+  //       ref={targetMedium}
+  //       className="flex flex-col items-start justify-start max-w-[420px] p-8 opacity-0"
+  //     >
+  //       <p className="block mt-4 text-2xl text-black">
+  //         Linas Thoemke is a fashion photographer based in Amsterdam. He started
+  //         working for small indie publications back in the mid-00s, and gathered
+  //         a following that slowly attracted the attention of bigger brands.
+  //       </p>
+  //       <br></br>
+  //       <p className="block mt-4 text-2xl">
+  //         Linas has worked on a wide array of different projects. Available to
+  //         work worldwide.
+  //       </p>
+  //       <Link
+  //         href="https://linasyfhpf.zyrosite.com/clients"
+  //         className="font-medium mt-4 text-2xl relative"
+  //       >
+  //         CLIENT LIST →
+  //         <span className="absolute left-0 right-0 bottom-1 h-[2px] bg-black transform"></span>
+  //       </Link>
+  //       <p className="font-medium text-2xl mt-12">EXHIBITIONS</p>
+  //       <div className="mt-10 ">
+  //         <p>THIS IS LINAS: ORIGIN STORY</p>
+  //         <p>2008, Amsterdam</p>
+  //       </div>
+  //       <div className="mt-10">
+  //         <p>TAKES ON COLOR AND SEA</p>
+  //         <p>2010, London and Amsterdam</p>
+  //       </div>
+  //       <div className="mt-10">
+  //         <p>EXPRESSIONS OF MORTALITY</p>
+  //         <p>2011, Amsterdam</p>
+  //       </div>
+  //       <div className="mt-10">
+  //         <p>THOEMKE & GEET: EXPLORING </p>
+  //         <p>2015-2016, Amsterdam and Paris</p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // ) : (
+  //   <div className="mx-auto w-full flex justify-center items-center text-black pt-10 pb-32">
+  //     <div
+  //       ref={targetMedium}
+  //       className="container max-w-6xl flex justify-between  relative mt-12 opacity-0 "
+  //     >
+  //       <div className="w-[37%]">
+  //         <p className="block mt-4 text-2xl">
+  //           Linas Thoemke is a fashion photographer based in Amsterdam. He
+  //           started working for small indie publications back in the mid-00s,
+  //           and gathered a following that slowly attracted the attention of
+  //           bigger brands.
+  //         </p>
+  //         <br></br>
+  //         <p className="block mt-4 mb-4 text-2xl">
+  //           Linas has worked on a wide array of different projects. Available to
+  //           work worldwide.
+  //         </p>
+  //         <Link
+  //           href="https://linasyfhpf.zyrosite.com/clients"
+  //           className="font-medium border-b-2 border-black text-2xl "
+  //         >
+  //           CLIENT LIST →
+  //         </Link>
+  //       </div>
+  //       <div className="w-[30%] mr-36 mt-2">
+  //         <p className="font-medium text-2xl">EXHIBITIONS</p>
+  //         <div className="mt-10 ">
+  //           <p>THIS IS LINAS: ORIGIN STORY</p>
+  //           <p>2008, Amsterdam</p>
+  //         </div>
+  //         <div className="mt-10">
+  //           <p>TAKES ON COLOR AND SEA</p>
+  //           <p>2010, London and Amsterdam</p>
+  //         </div>
+  //         <div className="mt-10">
+  //           <p>EXPRESSIONS OF MORTALITY</p>
+  //           <p>2011, Amsterdam</p>
+  //         </div>
+  //         <div className="mt-10">
+  //           <p>THOEMKE & GEET: EXPLORING </p>
+  //           <p>2015-2016, Amsterdam and Paris</p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default SecondBlockContent;
