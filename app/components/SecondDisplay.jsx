3dc7fac91efa1641f421297ac1674ed32d595d7e
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2024-01-08 17:12:06 +0100

    remake-seconddisplay

--- app/components/SecondDisplay.jsx
+++ app/components/SecondDisplay.jsx
@@ -1,76 +1,49 @@
-import { useRef } from "react";
-import useIntersectionObserver from "../hooks/useIntersectionObserver";
+const SecondDisplay = () => {
+  const images = [
+    "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1080,h=1472,fit=crop/linasyfhpf/Home_4-YKbxwZe9P2uRE8Va.png",
+    "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1080,h=1559,fit=crop/linasyfhpf/Home_5-ALpGwawkbqulDkvq.png",
+  ];
 
-const SecondDisplay = ({ screen }) => {
-  const targetMedium = useRef(null);
-
-  const animationsAndRefs = {
-    refsArray: [targetMedium],
-    animationNamesArray: ["animate-slideUp-medium"],
-  };
-
-  useIntersectionObserver(animationsAndRefs);
-
-  return screen <= 1152 ? (
-    <div className="mx-auto ">
-      <div className="flex w-full justify-center items-center bg-white items-left p-8">
-        <div ref={targetMedium} className="mt-20 mb-20 opacity-0">
-          <h1 className="text-black font-bold text-6xl">01</h1>
-          <h4 className="font-semibold text-black pt-5">HEAD OVER HEELS</h4>
-          <a className="cursor-pointer underline text-black pt-5">
-            Learn more about the project →
-          </a>
-          <img
-            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1080,h=1472,fit=crop/linasyfhpf/Home_4-YKbxwZe9P2uRE8Va.png"
-            alt="Model1"
-            className="w-[328px] h-[447px] mt-10 object-contain"
-          />
-          <img
-            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1080,h=1559,fit=crop/linasyfhpf/Home_5-ALpGwawkbqulDkvq.png"
-            alt="Model2"
-            className="w-[257px] h-[371px] pt-10 object-contain"
-          />
-          <h4 className="text-black font-medium mt-10">
+  return (
+    <div className="mx-auto bg-white w-full h-[100vh] flex flex-col justify-center items-center max-[1124px]:h-full">
+      <div className="container mx-auto w-full flex max-w-6xl max-[1124px]:w-[350px] max-[1124px]:flex-col relative">
+        <h1 className="text-8xl text-black absolute z-[1] mt-10 ml-10 max-[1124px]:order-0 max-[1124px]:relative max-[1124px]:ml-0 mb-5 max-[1124px]:text-7xl font-semibold">
+          01
+        </h1>
+        <img
+          src={images[0]}
+          alt="model1"
+          className="object-cover w-[65%] static z-[0] max-[1124px]:w-[110%] max-[1124px]:order-3 max-[1124px]:ml-2 max-[1124px]:mt-[20px]"
+        />
+        <h2 className="text-black font-light absolute z-[1] bottom-10 left-10 max-[1124px]:relative max-[1124px]:left-2 mt-10 max-[1124px]:order-4 ">
+          CLIENT: "DARE" shoes
+        </h2>
+        <div className="ml-10 max-[1124px]:flex flex-col justify-center items-center max-[1124px]:ml-2 max-[1124px]:order-2">
+          <h2 className="text-black font-light ml-4 h-[50px] max-[1124px]:hidden">
             Model: Anna Fenna <br />
             MUA: Jessie Gein
-          </h4>
-          <p className="text-black font-mdeium pt-4">CLIENT: "DARE" shoes</p>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className="mx-auto flex justify-center items-center bg-white ">
-      <div className="container w-full flex justify-center static mt-20 mb-20 max-w-6xl">
-        <div ref={targetMedium} className="relative flex opacity-0">
-          <h1 className="text-bold font-sans text-8xl text-black z-[1] absolute mt-5 ml-5">
-            01
-          </h1>
-          <p className="text-black font-sans text-medium absolute z-[1] flex bottom-10 left-10 lg:bottom-20">
-            CLIENT: "DARE" shoes
-          </p>
+          </h2>
           <img
-            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1418,h=1610,fit=crop/linasyfhpf/Home_4-YKbxwZe9P2uRE8Va.png"
-            alt="Nodel1"
-            className="w-[65%] static object-contain"
+            src={images[1]}
+            alt="Model2"
+            className="object-cover w-[100%] mt-20 ml-4 max-[1124px]:w-[80%]  max-[1124px]:hidden"
           />
-          <div className="ml-10">
-            <h4 className="text-black font-medium">
-              Model: Anna Fenna <br />
-              MUA: Jessie Gein
-            </h4>
-            <img
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=800,h=1138,fit=crop/linasyfhpf/Home_5-ALpGwawkbqulDkvq.png"
-              alt="Model2"
-              className="w-[100%] mt-20 object-conatain"
-            />
-            <div className="mt-[120px]">
-              <h4 className="text-black font-semibold ">HEAD OVER HEELS</h4>
-              <a className="text-black underline font-medium mt-5">
-                Learn more about the project →
-              </a>
-            </div>
-          </div>
+          <p className="font-semibold text-black mt-[220px] max-[1124px]:order-first max-[1124px]:mt-0 max-[1124px]:mr-[185px] ml-4 max-[1124px]:ml-[0px]">
+            HEAD OVER HEELS
+          </p>
+          <p className="font-light text-black ml-4 underline mt-2 max-[1124px]:order-second max-[1124px]:mr-[110px] max-[1124px]:ml-[0px] ">
+            Learn more about the project →
+          </p>
         </div>
+        <img
+          src={images[1]}
+          alt="Model2"
+          className="mt-10 max-[1124px]:block hidden order-3 max-[1124px]:w-[80%] max-[1124px]:ml-[8px] "
+        />
+        <h2 className="hidden max-[1124px]:block text-black font-light h-[50px] ml-2 mt-3 max-[1124px]:order-3 ">
+          Model: Anna Fenna <br />
+          MUA: Jessie Gein
+        </h2>
       </div>
     </div>
   );
