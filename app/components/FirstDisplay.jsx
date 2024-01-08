db1abf6a694f7be56c7c280245e15141703a2820
Danil <grechkindanil322@gmail.com>
2024-01-08 03:18:35 +0100

    fix FirstDisplay.jsx

--- app/components/FirstDisplay.jsx
+++ app/components/FirstDisplay.jsx
@@ -1,88 +1,34 @@
 import React, { useRef } from "react";
 import useIntersectionObserver from "../hooks/useIntersectionObserver";
 
-const FirstDisplay = ({ screen }) => {
-  const targetLarg = useRef(null);
-  const targetMedium = useRef(null);
-
-  const animationsAndRefs = {
-    refsArray: [targetLarg, targetMedium],
-    animationNamesArray: ["animate-slideUp-large", "animate-slideUp-medium"],
-  };
-
-  useIntersectionObserver(animationsAndRefs);
-
-  return screen <= 1152 ? (
-    <div className="w-full flex justify-center items-center mx-auto h-svh bg-[#04111b] pt-10">
-      <div
-        ref={targetLarg}
-        className="flex flex-col max-w-[420px] justify-center items-left p-8 pb-40"
-      >
-        <h3
-          className="font-bold word-wrap block"
-          style={{ fontSize: "80px", color: "white" }}
-        >
-          Linas
-        </h3>
-        <p>FASHION PHOTOGRAPHY | AMSTERDAM</p>
-        <p>THOEMKE@LINAS.COM</p>
-        <img
-          className="my-4"
-          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=503,h=673,fit=crop/linasyfhpf/Home_3-YBgxMPMkXLHyZX5g.png"
-          alt="image3"
-        />
-        <div className="w-full flex justify-between ">
-          <div className="w-[48%]">
-            <img
-              className="w-full h-auto"
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=194,h=201,fit=crop/linasyfhpf/Home_2-m6L1x6xeZetk65zO.png"
-              alt="image2"
-            />
-          </div>
-
-          <div className="w-[48%]">
-            <img
-              className="w-full h-full"
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=297,h=201,fit=crop/linasyfhpf/Home_1-mjEDk3krBEINqzOl.png"
-              alt="image1"
-            />
-          </div>
+const FirstDisplay = () => {
+  return (
+    <div className="w-full mx-auto bg-[#04111b]  p-12">
+      <div className="conteiner p-16 h-full mx-auto lg:grid grid-rows-3  grid-cols-2 gap-x-40 gap-y-2 grid-flow-col-dense max-w-7xl">
+        <div className=" flex row-span-1 w-max h-[90%]">
+          <img
+            className="object-cover w-[65%]"
+            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=297,h=201,fit=crop/linasyfhpf/Home_1-mjEDk3krBEINqzOl.png"
+            alt="image1"
+          />
+          <img
+            className="object-cover ml-4"
+            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=194,h=201,fit=crop/linasyfhpf/Home_2-m6L1x6xeZetk65zO.png"
+            alt="image2"
+          />
         </div>
-      </div>
-    </div>
-  ) : (
-    <div className="w-full mx-auto bg-[#04111b] pb-32 pt-16">
-      <div className="conteiner max-w-6xl mx-auto flex justify-between">
-        <div ref={targetMedium} className="flex flex-col justify-between">
-          <div className="flex space-x-3">
-            <img
-              className="object-cover"
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=297,h=201,fit=crop/linasyfhpf/Home_1-mjEDk3krBEINqzOl.png"
-              alt="image1"
-            />
-
-            <img
-              className="object-cover"
-              src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=194,h=201,fit=crop/linasyfhpf/Home_2-m6L1x6xeZetk65zO.png"
-              alt="image2"
-            />
-          </div>
-          <div>
-            <h3
-              className="font-bold word-wrap block"
-              style={{ fontSize: "200px" }}
-            >
-              Linas
-            </h3>
-            <div className="flex justify-between">
-              <p>FASHION PHOTOGRAPHY | AMSTERDAM</p>
-              <p>THOEMKE@LINAS.COM</p>
-            </div>
+        <div className=" row-span-2 flex flex-col justify-between">
+          <h3 className=" min-h-[50px]  text-[200px] font-bold mt-[90px]">
+            Linas
+          </h3>
+          <div className=" min-h-[50px] flex items-end">
+            <p>FASHION PHOTOGRAPHY | AMSTERDAM</p>
+            <p className="ml-8">THOEMKE@LINAS.COM</p>
           </div>
         </div>
 
         <img
-          ref={targetLarg}
+          className="flex justify-end row-span-3"
           src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=503,h=673,fit=crop/linasyfhpf/Home_3-YBgxMPMkXLHyZX5g.png"
           alt="image3"
         />
@@ -90,5 +36,34 @@ const FirstDisplay = ({ screen }) => {
     </div>
   );
 };
+
+{
+  /* <div className="flex  border-2 border-white  row-span-1">
+          <img
+            className="object-cover"
+            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=297,h=201,fit=crop/linasyfhpf/Home_1-mjEDk3krBEINqzOl.png"
+            alt="image1"
+          />
+          <img
+            className="object-cover"
+            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=194,h=201,fit=crop/linasyfhpf/Home_2-m6L1x6xeZetk65zO.png"
+            alt="image2"
+          />
+        </div>
+
+        <h3 className=" font-bold text-[200px] h-min max-[1152px]:text-[180px]">
+          Linas
+        </h3>
+
+        <div className="  max-[1152px]:text-[14px]">
+          <p>FASHION PHOTOGRAPHY | AMSTERDAM</p>
+          <p className="ml-4">THOEMKE@LINAS.COM</p>
+        </div>
+        <img
+          className=" border-2 border-white object-cover  max-[1152px]:h-[73vh]"
+          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=503,h=673,fit=crop/linasyfhpf/Home_3-YBgxMPMkXLHyZX5g.png"
+          alt="image3"
+        /> */
+}
 
 export default FirstDisplay;
--- app/page.js
+++ app/page.js
@@ -11,7 +11,10 @@ import Footer from "./components/Footer";
 
 
 const page = () => {
-  const { wrapperStyle, screen} =  useScreenHandling();
+  const { screen} =  useScreenHandling(); 
+  
+  console.log("screen", screen);
+
   if ( screen === 0){
     return <div></div>
   }
@@ -19,16 +22,16 @@ const page = () => {
   return (
       <main className="flex overflow-hidden max-h-screen flex-col w-full items-center">       
         <div
-          className={`${wrapperStyle} overflow-auto max-h-screen transition-all duration-500`}
+          className={`w-full overflow-auto max-h-screen transition-all duration-500`}
         >
           <Navbar screen={screen} />
           <FirstDisplay screen={screen}  />
-          <SecondDisplay screen={screen} />
+          {/* <SecondDisplay screen={screen} />
           <ThirdDisplay screen={screen}  />
           <FourthDisplay screen={screen}  />
           <FifthDisplay screen={screen}  />
           <SixthDisplay screen={screen}  />
-          <Footer screen={screen}  />
+          <Footer screen={screen}  /> */}
         </div>
       </main>
     );  
