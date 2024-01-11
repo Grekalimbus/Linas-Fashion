dccc75b9779acdd36e2e9ff39bd7d7bf0a8112c0
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2024-01-11 02:03:27 +0100

    add animation to thirddisplay

--- app/components/ThirdDisplay.jsx
+++ app/components/ThirdDisplay.jsx
@@ -1,3 +1,4 @@
+import AnimateWrap from "./AnimateWrap";
 const ThirdDisplay = () => {
   const images = [
     "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1006,h=1582,fit=crop/linasyfhpf/Home_8-m5KgxNx9kwhwwqNW.png",
@@ -8,30 +9,39 @@ const ThirdDisplay = () => {
     <div className="mx-auto  w-full h-[110vh] flex flex-col justify-center items-center max-[1124px]:h-full bg-[#04111b]  py-20">
       <div className="container mx-auto w-full flex max-w-6xl max-[1124px]:w-[350px] max-[1124px]:flex-col relative max-[1124px]:justify-between">
         <div className="flex flex-col ">
-          <h1 className="text-white font-bold text-8xl w-[100px] max-[1124px]:text-7xl ">
+          <AnimateWrap className="text-white font-bold text-8xl w-[100px] max-[1124px]:text-7xl ">
             02
-          </h1>
-          <img
+          </AnimateWrap>
+          <AnimateWrap
+            tag="img"
             src={images[2]}
             alt="model"
             className="relative w-[75%]  z-[0] object-cover mt-[10px] ml-[160px] max-[1124px]:order-2 max-[1124px]:ml-0 max-[1124px]:mt-10"
           />
-          <img
+          <AnimateWrap
+            tag="img"
             src={images[1]}
             alt="flower"
             className=" flex z-[1] w-[25%] object-cover absolute top-[260px] max-[1124px]:order-2 max-[1124px]:z-[0] max-[1124px]:top-0 max-[1124px]:relative max-[1124px]:mt-10 max-[1124px]:w-[60%]"
           />
-          <h1 className="font-semibold mt-20 max-[1152px]:mt-10">
+          <AnimateWrap
+            tag="h1"
+            className="font-semibold mt-20 max-[1152px]:mt-10"
+          >
             FLOWER GARDEN
-          </h1>
-          <p className="font-light mt-1 underline  ">
+          </AnimateWrap>
+          <AnimateWrap tag="p" className="font-light mt-1 underline  ">
             Learn more about the project →
-          </p>
-          <p className=" font-light w-[140px] absolute z-[1] right-10 bottom-10 max-[1124px]:right-20 max-[1124px]:bottom-4">
+          </AnimateWrap>
+          <AnimateWrap
+            tag="p"
+            className=" font-light w-[140px] absolute z-[1] right-10 bottom-10 max-[1124px]:right-20 max-[1124px]:bottom-4"
+          >
             Model: Jake James
-          </p>
+          </AnimateWrap>
         </div>
-        <img
+        <AnimateWrap
+          tag="img"
           src={images[0]}
           alt="model1"
           className="w-[47%] ml-20 static max-[1124px]:order-0 max-[1124px]:mt-10 max-[1124px]:w-[80%] max-[1124px]:ml-0"
