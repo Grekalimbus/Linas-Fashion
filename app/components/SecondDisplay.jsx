d085b0dce06ebb9ea51db149f1aa2538367c7faa
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2024-01-11 01:56:06 +0100

    add animation to second component

--- app/components/SecondDisplay.jsx
+++ app/components/SecondDisplay.jsx
@@ -1,3 +1,4 @@
+import AnimateWrap from "./AnimateWrap";
 const SecondDisplay = () => {
   const images = [
     "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1080,h=1472,fit=crop/linasyfhpf/Home_4-YKbxwZe9P2uRE8Va.png",
@@ -7,43 +8,61 @@ const SecondDisplay = () => {
   return (
     <div className="mx-auto bg-white w-full h-[110vh] flex flex-col justify-center items-center max-[1124px]:h-full ">
       <div className="container mx-auto w-full flex max-w-6xl max-[1124px]:w-[350px] max-[1124px]:flex-col relative py-20">
-        <h1 className="text-8xl text-black absolute z-[1] mt-10 ml-10 max-[1124px]:order-0 max-[1124px]:relative max-[1124px]:ml-0 mb-5 max-[1124px]:text-7xl font-semibold max-[1124px]:mt-0 ">
+        <AnimateWrap
+          tag="h1"
+          className="text-8xl text-black absolute z-[1] mt-10 ml-10 max-[1124px]:order-0 max-[1124px]:relative max-[1124px]:ml-0 mb-5 max-[1124px]:text-7xl font-semibold max-[1124px]:mt-0 "
+        >
           01
-        </h1>
-        <img
+        </AnimateWrap>
+
+        <AnimateWrap
+          tag="img"
           src={images[0]}
           alt="model1"
           className="object-cover w-[60%] static z-[0] max-[1124px]:w-[110%] max-[1124px]:order-3 max-[1124px]:ml-2 max-[1124px]:mt-[20px]"
         />
-        <h2 className="text-black font-light absolute z-[1] bottom-[110px] left-10 max-[1124px]:relative max-[1124px]:left-1.5 mt-10 max-[1124px]:order-4 max-[1124px]:z-[0] max-[1124px]:mt-[110px]">
+        <AnimateWrap className="text-black font-light absolute z-[1] bottom-[110px] left-10 max-[1124px]:relative max-[1124px]:left-1.5 mt-10 max-[1124px]:order-4 max-[1124px]:z-[0] max-[1124px]:mt-[110px]">
           CLIENT: "DARE" shoes
-        </h2>
+        </AnimateWrap>
         <div className="ml-10 max-[1124px]:flex flex-col justify-center items-center max-[1124px]:ml-2 max-[1124px]:order-2">
-          <h2 className="text-black font-light ml-4 h-[50px] max-[1124px]:hidden">
+          <AnimateWrap
+            tag="p"
+            className="text-black font-light ml-4 h-[50px] max-[1124px]:hidden"
+          >
             Model: Anna Fenna <br />
             MUA: Jessie Gein
-          </h2>
-          <img
+          </AnimateWrap>
+          <AnimateWrap
+            tag="img"
             src={images[1]}
             alt="Model2"
             className="object-cover w-[90%] mt-20 ml-4 max-[1124px]:w-[80%]  max-[1124px]:hidden"
           />
-          <p className="font-semibold text-black mt-[220px] ml-4 max-[1124px]:order-first max-[1124px]:mt-0 max-[1124px]:mr-[185px]  max-[1124px]:ml-[0px]">
+          <AnimateWrap
+            tag="p"
+            className="font-semibold text-black mt-[220px] ml-4 max-[1124px]:order-first max-[1124px]:mt-0 max-[1124px]:mr-[185px]  max-[1124px]:ml-[0px]"
+          >
             HEAD OVER HEELS
-          </p>
-          <p className="font-light text-black ml-4 underline mt-2 max-[1124px]:order-second max-[1124px]:mr-[110px] max-[1124px]:ml-[0px] ">
+          </AnimateWrap>
+          <AnimateWrap
+            tag="p"
+            className="font-light text-black ml-4 underline mt-2 max-[1124px]:order-second max-[1124px]:mr-[110px] max-[1124px]:ml-[0px] "
+          >
             Learn more about the project →
-          </p>
+          </AnimateWrap>
         </div>
-        <img
+        <AnimateWrap
           src={images[1]}
           alt="Model2"
           className="mt-10 max-[1124px]:block hidden order-3 max-[1124px]:w-[80%] max-[1124px]:ml-[8px] "
         />
-        <h2 className="hidden max-[1124px]:block text-black font-light h-[50px] ml-2 mt-3 max-[1124px]:order-3 ">
+        <AnimateWrap
+          tag="h2"
+          className="hidden max-[1124px]:block text-black font-light h-[50px] ml-2 mt-3 max-[1124px]:order-3 "
+        >
           Model: Anna Fenna <br />
           MUA: Jessie Gein
-        </h2>
+        </AnimateWrap>
       </div>
     </div>
   );
